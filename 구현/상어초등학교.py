6fe01cee2ca57f519f6563b31368374b0e480438
Lucy <64342804+Na-Hyeon-Oh@users.noreply.github.com>
2023-02-04 11:06:07 +0900

    Update 상어초등학교.py

--- 구현/상어초등학교.py
+++ 구현/상어초등학교.py
@@ -14,14 +14,63 @@
 * 제한 시간 1초
 '''
 
+
 import sys
 
 def input():
   return sys.stdin.readline()
 
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+
 n = int(input())
 students = [list(map(int, input().split())) for _ in range(n*n)]
 
+seats = [[0 for _ in range(n)] for _ in range(n)]
+for student in students:
+  id = student[0]
+  adjacency = []             # (좋아하는 학생 수, 비어있는 칸 수, -r, -c)
+  for r in range(n):
+    for c in range(n):
+      likes = 0
+      empties = 0
+      for i in range(4):     # 인접한 칸 탐색
+        nx = c + dx[i]
+        ny = r + dy[i]
+        if 0 <= nx < n and 0 <= ny < n:
+          if seats[ny][nx] == 0: empties += 1
+          else:
+            for s in range(1, 5):
+              if seats[ny][nx] == student[s]: 
+                likes += 1
+                break
+      adjacency.append([likes, empties, -r, -c])
+  adjacency.sort(reverse=True)
+  for element in adjacency:
+    if seats[-element[2]][-element[3]] == 0: 
+      seats[-element[2]][-element[3]] = id
+      break
+
+result = 0
+for r in range(n):
+  for c in range(n):
+    for student in students:
+      if student[0] == seats[r][c]:
+        likes = 0
+        for i in range(4):
+          nx = c + dx[i]
+          ny = r + dy[i]
+          if 0 <= nx < n and 0 <= ny < n:
+            for s in range(1, 5):
+              if seats[ny][nx] == student[s]: 
+                likes += 1
+                break
+        if likes == 1: result += 1
+        elif likes == 2: result += 10
+        elif likes == 3: result += 100
+        elif likes == 4: result += 1000
+
+print(result)
 
 '''
 
