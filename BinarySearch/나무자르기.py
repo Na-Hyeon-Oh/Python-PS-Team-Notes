5ac56d9f54f5ed3a694441db64144086887d8f97
Lucy <64342804+Na-Hyeon-Oh@users.noreply.github.com>
2023-02-15 16:35:10 +0900

    Update 나무자르기.py

--- BinarySearch/나무자르기.py
+++ BinarySearch/나무자르기.py
@@ -6,6 +6,9 @@
 * 제한 시간 1초
 '''
 
+
+# i. PyPy3
+
 import sys
 
 def input():
@@ -14,10 +17,11 @@ def input():
 n, m = map(int, input().split())
 h = list(map(int, input().split()))
 
-h.sort()
+#h.sort()
 result = 0
-left = 0
-right = h[-1]
+left = 1
+right = max(h)
+#right = h[-1]
 while left <= right:
   mid = (left + right) // 2
   sum = 0
@@ -30,8 +34,32 @@ while left <= right:
     right = mid - 1
 print(result)
 
+
+# ii. Python3
+
+import sys
+
+def input():
+  return sys.stdin.readline().rstrip()
+
+n, m = map(int, input().split())
+h = list(map(int, input().split()))
+
+result = 0
+left = 1
+right = max(h)
+while left <= right:
+  mid = (left + right) // 2
+  if sum(target - mid for target in h if target - mid > 0) >= m:
+    result = mid
+    left = mid + 1  
+  else: 
+    right = mid - 1
+print(result)
+
+
         
 '''
 - 가능한 M의 범위가 10억으로 매우 크므로 이진 탐색 사용하여야 함
-- PyPy3가 아닌 Python3으로는 시간 초과
+- i.으로는 PyPy3가 아닌 Python3으로는 시간 초과 -> sum함수를 사용하는 것이 더 
 '''
