2cf837a8740d8ca54519b9fd20455339271ef586
Lucy <64342804+Na-Hyeon-Oh@users.noreply.github.com>
2023-02-15 10:49:18 +0900

    Create 나무자르기.py

---
+++ BinarySearch/나무자르기.py
@@ -0,0 +1,36 @@
+# 나무 자르기
+# https://www.acmicpc.net/problem/2805
+'''
+나무가 N개 있을 때, 적어도 M미터의 나무를 집에 가져가기 위해 절단기에 설정할 수 있는 높이의 최댓값
+* 1 <= N <= 1,000,000, 1 <= M <= 2,000,000,000
+* 제한 시간 1초
+'''
+
+import sys
+
+def input():
+  return sys.stdin.readline().rstrip()
+
+n, m = map(int, input().split())
+h = list(map(int, input().split()))
+
+h.sort()
+result = 0
+left = 0
+right = h[-1]
+while left <= right:
+  mid = (left + right) // 2
+  sum = 0
+  for tree in h:
+    if tree - mid > 0: sum += tree - mid
+  if sum >= m:
+    result = mid
+    left = mid + 1  
+  else: 
+    right = mid - 1
+print(result)
+
+        
+'''
+- PyPy3가 아닌 Python3으로는 시간 초과
+'''
