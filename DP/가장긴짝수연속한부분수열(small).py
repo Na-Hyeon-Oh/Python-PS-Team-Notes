69eaec1c878e0518257d53908453274a68c29177
Lucy <64342804+Na-Hyeon-Oh@users.noreply.github.com>
2023-03-09 14:17:34 +0900

    Update 가장긴짝수연속한부분수열(small).py

--- DP/가장긴짝수연속한부분수열(small).py
+++ DP/가장긴짝수연속한부분수열(small).py
@@ -31,7 +31,7 @@ print(max(dp))
 
 # ii. 
 
-dp = [0]                  # dp[i]: i번째 수로 시작하는 수열의 최대 짝수 개수
+dp = [0]                  # dp[i]: (i+1)번째 홀수 전의 짝수 개
 for i in range(n):
   if s[i] % 2 == 0: dp[-1] += 1          # 짝수 : 마지막에 1 더하기
   else: dp.append(dp[-1])                # 홀수 : 마지막 원소 추가하기
