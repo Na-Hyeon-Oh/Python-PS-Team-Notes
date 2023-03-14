943e6cb103e4a74b2330f96968e3599cd63e79ea
Lucy <64342804+Na-Hyeon-Oh@users.noreply.github.com>
2023-03-14 11:20:11 +0900

    Create 쉬운계단수.py

---
+++ DP/쉬운계단수.py
@@ -0,0 +1,33 @@
+# [B_S1] 쉬운 계단 수
+# https://www.acmicpc.net/problem/10844
+'''
+길이가 N(1 <= N <= 100)인 계단수의 개수를 1,000,000,000으로 나눈 나머지 출력
+* 계단수: 인접한 모든 자리의 차이가 1인 수   ex. 12345
+* 시간 제한 1초
+* 메모리 제한 256MB
+'''
+
+import sys
+
+def input():
+  return sys.stdin.readline()
+
+n = int(input())
+
+dp = [[0 for j in range(10)] for i in range(n)]              # dp[i][j]: i 길이의 수에서 앞에오는 숫자의 개수
+for i in range(1, 10):
+  dp[0][i] = 1
+for i in range(1, n):
+  for j in range(10):
+    target = dp[i - 1][j]
+    if j < 9: dp[i][j + 1] += target
+    if j > 0: dp[i][j - 1] += target
+
+result = 0
+for i in range(10):
+  result += dp[n - 1][i]
+print(result % 1000000000)
+
+'''
+
+'''
