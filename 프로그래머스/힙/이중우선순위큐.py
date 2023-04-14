127c6b4efa423b91057deb3264c30733eeb8439f
Lucy <64342804+Na-Hyeon-Oh@users.noreply.github.com>
2023-04-14 19:53:05 +0900

    Update 이중우선순위큐.py

--- 프로그래머스/힙/이중우선순위큐.py
+++ 프로그래머스/힙/이중우선순위큐.py
@@ -19,32 +19,4 @@ def solution(operations):
     if len(queue) > 0: answer = [max(queue), min(queue)]
     return answer
   
-# ii. 정석 (힙 사용)
-
-from heapq import heappush, heappop
-
-def solution(arguments):
-    max_heap = []
-    min_heap = []
-    for arg in arguments:
-        if arg == "D 1":
-            if max_heap != []:
-                heappop(max_heap)
-                if max_heap == [] or -max_heap[0] < min_heap[0]:
-                    min_heap = []
-                    max_heap = []
-        elif arg == "D -1":
-            if min_heap != []:
-                heappop(min_heap)
-                if min_heap == [] or -max_heap[0] < min_heap[0]:
-                    max_heap = []
-                    min_heap = []
-        else:
-            num = int(arg[2:])
-            heappush(max_heap, -num)
-            heappush(min_heap, num)
-    if min_heap == []:
-        return [0, 0]
-    return [-heappop(max_heap), heappop(min_heap)]
-
   
