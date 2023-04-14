bc888e63b6e8423c75fcbe5e418f18df3630267b
Lucy <64342804+Na-Hyeon-Oh@users.noreply.github.com>
2023-04-14 15:39:26 +0900

    Update 여행경로.py

--- 프로그래머스/BFS&DFS/여행경로.py
+++ 프로그래머스/BFS&DFS/여행경로.py
@@ -13,23 +13,26 @@ global answer
 answer = []
 
 def solution(tickets):
-    # 출발-도착 cities dictionary 만들기
+    # 출발-도착 cities dictionary 만들기 -> 오름차순 정렬 
     cities = dict()
     for [src, dest] in tickets:
         cities[src] = cities.get(src, [])
         cities[src].append(dest)
+    for city in cities:   
+        cities[city].sort()
 
-    dfs(len(tickets) + 1, ["ICN"], cities)      # dfs 이용 경로 탐색
-    answer.sort()                       # 오름차순 정렬
+    dfs(len(tickets) + 1, ["ICN"], cities)      # dfs 이용 경로 탐색                   
     return answer[0]
 
 def dfs(n, arr, cities):
+    if len(answer) >= 1: return
     if len(arr) == n: answer.append(arr)
     src = arr[-1]
     for dest in cities.get(src, []):
         tmpCities = deepcopy(cities)
         tmpCities[src].remove(dest)
         dfs(n, arr + [dest], tmpCities)
+        
         
 # ii. global 쓰지 않고 성능 개선
 
